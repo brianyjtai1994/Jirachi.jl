a72cb408e6ffacc3634b6d40ed556275f5669cc7
brianyjtai1994 <briantjtai1994@gmail.com>
2021-07-03 00:17:46 +0800

    Add: `VecI`, `VecO`, `VecB`, `MatI`, `MatO`, `MatB`

--- src/Jirachi.jl
+++ src/Jirachi.jl
@@ -1,5 +1,12 @@
 module Jirachi
 
+const VecI = AbstractVector; # Input  Vector
+const VecO = AbstractVector; # Output Vector
+const VecB = AbstractVector; # Buffer Vector
+const MatI = AbstractMatrix; # Input  Matrix
+const MatO = AbstractMatrix; # Output Matrix
+const MatB = AbstractMatrix; # Buffer Matrix
+
 include("./BLAS_Lv1.jl")
 include("./BLAS_Lv2.jl")
 include("./RungeKutta.jl")
